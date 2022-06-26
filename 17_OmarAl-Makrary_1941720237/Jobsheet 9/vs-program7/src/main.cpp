2d01ba9cbfcf9eef55ce3b78607eb923ad4e51fb
Omar630603 <omar.yem1111@gmail.com>
2022-06-26 21:52:42 +0700

    Jobsheet 9

---
+++ 17_OmarAl-Makrary_1941720237/Jobsheet 9/vs-program7/src/main.cpp
@@ -0,0 +1,108 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SimpleDHT.h>
+
+const char *ssid = "WI-FI_NAME";            
+const char *password = "Password";       
+const char *mqtt_server = "broker.hivemq.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+SimpleDHT11 dht11(D7);
+
+long now = millis();
+long lastMeasure = 0;
+String macAddr = "";
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+  macAddr = WiFi.macAddress();
+  Serial.println(macAddr);
+}
+
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(macAddr.c_str()))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("MQTT Node-RED");
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  pinMode(D4, OUTPUT);
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  if (!client.loop())
+  {
+    client.connect(macAddr.c_str());
+  }
+  now = millis();
+  if (now - lastMeasure > 5000)
+  {
+    lastMeasure = now;
+    int err = SimpleDHTErrSuccess;
+
+    byte temperature = 0;
+    byte humidity = 0;
+    if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess)
+    {
+      Serial.print("Reading DHT11 failed, err=");
+      Serial.println(err);
+      delay(1000);
+      return;
+    }
+    static char temperatureTemp[7];
+    dtostrf(temperature, 4, 2, temperatureTemp);
+    Serial.println(temperatureTemp);
+
+    static char humidityTemp[7];
+    dtostrf(humidity, 4, 2, humidityTemp);
+    Serial.println(humidityTemp);
+    bool onOff = digitalRead(D4);
+    digitalWrite(D4, !onOff);
+    static char onOffLight[7];
+    dtostrf(onOff, 4, 2, onOffLight);
+    Serial.println(onOffLight);
+
+    client.publish("room/light", onOffLight); 
+    client.publish("room/temperature", temperatureTemp); 
+    client.publish("room/humidity", humidityTemp); 
+  }
+}
